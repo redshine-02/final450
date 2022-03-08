10e3ff3ea4a4c80441b1f2b623f28db69ab0f6d5
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-03-08 16:31:01 +0530

    efficient O(logn)

--- Notes/BinarySearch/minimum_binary.java
+++ Notes/BinarySearch/minimum_binary.java
@@ -37,3 +37,32 @@ public class minimum_binary {
 
     }
 }
+// int n = arr.length;
+//         int start = 0;
+//         int end = n - 1;
+//         while(start<=end){
+//             int mid = start + (end - start)/2;
+//            // mid = (mid+n)%n;
+//             if(arr[mid] == target){
+//                 return mid;
+//             }
+//             else if(arr[mid]>=arr[start]){
+//                 if(target>=arr[start] && target<=arr[mid]){
+//                     end = mid-1;
+//                 }
+//                 else{
+//                     start = mid+1;
+//                 }
+//             }
+//             else{
+//                 if(target >=arr[mid] && target <=arr[end]){
+//                     //end = mid-1;
+//                     start = mid+1;
+//                 }
+//                 else{
+//                    // start = mid+1;
+//                     end = mid-1;
+//                 }
+//             }
+//         }
+//         return -1;
--- Notes/BinarySearch/search_rotated.java
+++ Notes/BinarySearch/search_rotated.java
@@ -1,7 +1,36 @@
 package Notes.BinarySearch;
 import java.util.*;
 
+
 public class search_rotated {
+    public int search(int[] arr, int target) {
+        int n = arr.length;
+        int lo = 0;
+        int hi = n-1;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(arr[mid]==target){
+               return mid; 
+            }
+            else if(arr[mid]>=arr[lo]){
+                if(target>=arr[lo] && target<=arr[mid]){
+                    hi = mid-1;
+                }
+                else{
+                    lo = mid+1;
+                }
+            }
+            else {
+                if(target>=arr[mid] && target<=arr[hi]){
+                    lo = mid+1;
+                }
+                else{
+                    hi = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
     public static int binarySearch(int[] arr,int n){
         int start = 0;
         int end = n-1;
